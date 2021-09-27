2d5d15648e5687a0bec17a5f78db165078f3a51a
ManuelExposito <manuelexposito4d@gmail.com>
2021-09-27 14:24:25 +0200

    Apuntes añadidos

--- 01_PrimerApiRest/src/main/java/com/salesianostriana/dam/_PrimerApiRest/TaskController.java
+++ 01_PrimerApiRest/src/main/java/com/salesianostriana/dam/_PrimerApiRest/TaskController.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.dam._PrimerApiRest;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +16,24 @@ public class TaskController {
 
 
     private final TaskRepository repository;
-
+    /*
     @GetMapping("/")
     public List<Task> findAll(){
 
         return repository.findAll();
 
+    }*/
+
+    //Otra forma de hacer el get para mostrarlos todos.
+    @GetMapping("/")
+    public ResponseEntity<List<Task>> findAll(){
+
+        return ResponseEntity.ok()
+                             .body(repository.findAll());
+
     }
+
+
 
     //Si pusieramos una de nuestras clases el código de respuesta sería 200, si necesitasemos otra,
     //usaremos ResponseEntity<T>
@@ -30,16 +42,29 @@ public class TaskController {
 
         //Esto nos permite modificar el código de estado de la respuesta (que antes salía 200 cuando
         // debía aparecer 201)
-        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(task));
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(repository.save(task));
     }
     //@RequestBody T t --> Necesita la estructura de la clase genérica que se le pasa.
+
+
+    /*
+
 
 
 
     @GetMapping("/{id}")
     public Task findOne(@PathVariable("id") Long id){
 
-        return repository.findById(id).orElse(null);
+        return repository.findById(id)
+                         .orElse(null);
+
+    }*/
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findOne(@PathVariable("id") Long id){
+
+        return ResponseEntity
+                .of(repository.findById(id));
 
 
     }
@@ -50,15 +75,17 @@ public class TaskController {
     public ResponseEntity<?> delete(@PathVariable("id") Long id){
 
         repository.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.noContent()
+                             .build();
         //también serviría --> ResponseEntity.status(204).build();
     }
 
-
+    /*
     @PutMapping("/{id}")
     public Task edit(@RequestBody Task task, @PathVariable Long id){
 
-        Task old = repository.findById(id).orElse(task);
+        Task old = repository.findById(id)
+                             .orElse(task);
 
         old.setText(task.getText());
         old.setTitle(task.getTitle());
@@ -66,7 +93,23 @@ public class TaskController {
 
         return repository.save(old);
 
+    }*/
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> edit(@RequestBody Task task, @PathVariable Long id) {
+
+
+        return ResponseEntity.of(repository.findById(id).map(x -> {
+                    x.setTitle(task.getTitle());
+                    x.setText(task.getText());
+
+                    repository.save(x);
+                    return x;
+                })
+        );
     }
+
+
 
 
 }
