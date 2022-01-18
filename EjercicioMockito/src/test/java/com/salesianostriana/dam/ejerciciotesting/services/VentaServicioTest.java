9e0fc5ed7cb958f3c51b527495e846f4c55047a4
ManuelExposito <manuelexposito4d@gmail.com>
2022-01-18 14:27:28 +0100

    ejercicio testing

---
+++ EjercicioMockito/src/test/java/com/salesianostriana/dam/ejerciciotesting/services/VentaServicioTest.java
@@ -0,0 +1,30 @@
+package com.salesianostriana.dam.ejerciciotesting.services;
+
+import com.salesianostriana.dam.ejerciciotesting.repos.Repositorio;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VentaServicioTest {
+
+    @Mock
+    Repositorio repositorio;
+
+
+    @Test
+    void nuevaVenta() {
+
+
+
+
+    }
+
+    @Test
+    void addProductoToVenta() {
+    }
+
+    @Test
+    void removeLineaVenta() {
+    }
+}
